223036d3b00d8a61c69a7e22f30b23829cc09722
WesleySherman <wesley.sherman@expsouthafrica.co.za>
2024-12-12 14:16:10 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,779 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---reception-area",
+      "name": "01 - Reception Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6628402224696863,
+          "pitch": 0.40454549877890145,
+          "rotation": 0,
+          "target": "1-02---side-office"
+        },
+        {
+          "yaw": 1.7890396548370617,
+          "pitch": 0.36217409572011405,
+          "rotation": 0,
+          "target": "2-03---open-plan-office"
+        },
+        {
+          "yaw": -2.7446229147865147,
+          "pitch": 0.3390366346174538,
+          "rotation": 0,
+          "target": "4-05---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---side-office",
+      "name": "02 - Side Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.3513196391151308,
+        "pitch": -0.010080800403340362,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0738383505435767,
+          "pitch": 0.41449282517117325,
+          "rotation": 0,
+          "target": "0-01---reception-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---open-plan-office",
+      "name": "03 - Open Plan Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.045647072744468886,
+        "pitch": -0.0018797568267672204,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1561435790166037,
+          "pitch": 0.3647871738015276,
+          "rotation": 0,
+          "target": "3-04---boardroom"
+        },
+        {
+          "yaw": 2.647095241374732,
+          "pitch": 0.35142813715508936,
+          "rotation": 0,
+          "target": "0-01---reception-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---boardroom",
+      "name": "04 - Boardroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.0982599443037664,
+        "pitch": 0.020990169125346014,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.052465320646469,
+          "pitch": 0.21863697157522743,
+          "rotation": 0,
+          "target": "2-03---open-plan-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---passage",
+      "name": "05 - Passage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.2571551427659404,
+        "pitch": 0.02396960984300911,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8743360998016447,
+          "pitch": 0.11406231278448153,
+          "rotation": 4.71238898038469,
+          "target": "6-07---meeting-room-01"
+        },
+        {
+          "yaw": 2.2402503103399294,
+          "pitch": 0.12085385587388942,
+          "rotation": 0,
+          "target": "5-06---serverroom"
+        },
+        {
+          "yaw": 0.6864239371308258,
+          "pitch": 0.08182099986410485,
+          "rotation": 0,
+          "target": "9-10---back-office"
+        },
+        {
+          "yaw": -0.900467234993565,
+          "pitch": 0.002432252636012322,
+          "rotation": 0,
+          "target": "11-12---bathroom-02"
+        },
+        {
+          "yaw": 2.8483300751178016,
+          "pitch": 0.1279429776815455,
+          "rotation": 1.5707963267948966,
+          "target": "0-01---reception-area"
+        },
+        {
+          "yaw": -0.7517658042698336,
+          "pitch": -0.008209011461403293,
+          "rotation": 1.5707963267948966,
+          "target": "12-13---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---serverroom",
+      "name": "06 - Serverroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.899530321172266,
+        "pitch": 0.010449010725071872,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9194524423776507,
+          "pitch": 0.6714957241934698,
+          "rotation": 0,
+          "target": "4-05---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07---meeting-room-01",
+      "name": "07 - Meeting Room 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.06847872568530455,
+        "pitch": -0.009398784133836102,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5031079540184855,
+          "pitch": 0.38783664278733276,
+          "rotation": 0,
+          "target": "8-09---meeting-room-03"
+        },
+        {
+          "yaw": 0.5981544850386271,
+          "pitch": 0.39147139569912426,
+          "rotation": 0,
+          "target": "7-08---meeting-room-02"
+        },
+        {
+          "yaw": 1.6350181484086885,
+          "pitch": 0.45240000976029116,
+          "rotation": 0,
+          "target": "4-05---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08---meeting-room-02",
+      "name": "08 - Meeting Room 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 3.100405898620286,
+        "pitch": 0.03216900149542745,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.802244488560694,
+          "pitch": 0.17854275747709458,
+          "rotation": 0,
+          "target": "6-07---meeting-room-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09---meeting-room-03",
+      "name": "09 - Meeting Room 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.680974258828506,
+        "pitch": 0.00216172737710707,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.853054373483448,
+          "pitch": 0.11581980691095417,
+          "rotation": 0,
+          "target": "6-07---meeting-room-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10---back-office",
+      "name": "10 - Back Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.038952624196545216,
+        "pitch": 0.036896302221753885,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6041210091702887,
+          "pitch": 0.39529299662952155,
+          "rotation": 0,
+          "target": "4-05---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11---bathroom-01",
+      "name": "11 - Bathroom 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.023383894781528,
+        "pitch": 0.8035552700746642,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.960225499680069,
+          "pitch": 0.014945271166787677,
+          "rotation": 1.5707963267948966,
+          "target": "4-05---passage"
+        },
+        {
+          "yaw": 3.0318906031583968,
+          "pitch": 0.02069248897247178,
+          "rotation": 4.71238898038469,
+          "target": "12-13---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-12---bathroom-02",
+      "name": "12 - Bathroom 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.3189027021617008,
+        "pitch": 0.6688170857233864,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4679312039448273,
+          "pitch": 0.2713137012268927,
+          "rotation": 0,
+          "target": "4-05---passage"
+        },
+        {
+          "yaw": 2.5635689352985604,
+          "pitch": 0.7217446642240546,
+          "rotation": 4.71238898038469,
+          "target": "12-13---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-13---kitchen",
+      "name": "13 - Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5164510756788232,
+        "pitch": 0.061950058507104444,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0169679155639848,
+          "pitch": 0.17052710670157722,
+          "rotation": 0,
+          "target": "13-14---back-of-the-property"
+        },
+        {
+          "yaw": -2.6487447635458974,
+          "pitch": 0.047304421422214915,
+          "rotation": 4.71238898038469,
+          "target": "4-05---passage"
+        },
+        {
+          "yaw": -2.6447912576211756,
+          "pitch": 0.47510695924560054,
+          "rotation": 1.5707963267948966,
+          "target": "10-11---bathroom-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-14---back-of-the-property",
+      "name": "14 - Back of the Property",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.8349294935844966,
+        "pitch": 0.07759748827983515,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5346004470949843,
+          "pitch": 0.21752059699902837,
+          "rotation": 0,
+          "target": "15-16---storeroom-02"
+        },
+        {
+          "yaw": 3.127568818506955,
+          "pitch": 0.1730125863267098,
+          "rotation": 0,
+          "target": "14-15---storeroom-01"
+        },
+        {
+          "yaw": 2.806962170726056,
+          "pitch": 0.18484384686935584,
+          "rotation": 0,
+          "target": "12-13---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-15---storeroom-01",
+      "name": "15 - Storeroom 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.667424531631889,
+        "pitch": 0.11791201913358051,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0768247075540636,
+          "pitch": 0.1128030092071679,
+          "rotation": 0,
+          "target": "13-14---back-of-the-property"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-16---storeroom-02",
+      "name": "16 - Storeroom 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.8073810999798887,
+        "pitch": 0.059948674814991776,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1146191150121174,
+          "pitch": 0.12693274511826047,
+          "rotation": 0,
+          "target": "13-14---back-of-the-property"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "6th Avenue 37, Walmer",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
